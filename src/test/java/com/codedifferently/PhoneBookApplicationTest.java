928f7d171328d59b36199d9f3942042d4d3ab532
Samuel Kahsai Ghebremedhin <samuelka2001@yahoo.com>
2020-10-23 21:20:07 -0400

    Update PhoneBookApplicationTest.java

--- src/test/java/com/codedifferently/PhoneBookApplicationTest.java
+++ src/test/java/com/codedifferently/PhoneBookApplicationTest.java
@@ -1,4 +1,15 @@
 package com.codedifferently;
 
-public class PhoneBookApplicationTest {
+import java.io.Console;
+import java.util.Scanner;
+
+public class PhoneBookApplication {
+    public static void main(String[] args){
+        Console console = System.console();
+        Scanner fred = new Scanner(System.in);
+        console.printf("What is your name?");
+        String name = fred.next();
+        console.printf(" Your name is %s %n", name);
+    }
+
 }
